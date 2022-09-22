0119cdf39bc9b01abe0496a5aea805f6fbbac9c6
ndzd <n_level@mail.ru>
2022-09-22 23:11:16 +0300

    Task3

--- Task3/Program.cs
+++ Task3/Program.cs
@@ -8,3 +8,41 @@
 // Среднее арифметическое каждого столбца: 4,6; 5,6; 3,6; 3.
 
 
+System.Console.WriteLine("Введите количество элементов в строке: ");
+int m = Convert.ToInt32(Console.ReadLine());
+System.Console.WriteLine("Введите количество элементов в столбце: ");
+int n = Convert.ToInt32(Console.ReadLine());
+
+int GetResult(int m, int n)
+{
+    
+int [,] arr = new int [m,n];
+int [] result = new int [m];
+int sum = 0;
+int res = 0;
+    for (int i = 0; i < m; i++){
+        for (int j = 0; j < n; j++){
+            arr [i,j] = new Random().Next(10);
+            System.Console.Write($"{arr[i,j]} ");
+        }
+        System.Console.WriteLine();
+    }
+
+    System.Console.WriteLine();
+
+    for (int i = 0; i < n; i++){
+        sum += arr[i,j];
+        for (int j = 0; j < m; j++){
+            
+            
+            //System.Console.Write();
+        }
+        res = sum / m;
+        System.Console.Write(sum);
+        System.Console.Write(res);
+        System.Console.WriteLine();
+    }
+    return res;
+    
+}
+GetResult(m,n);

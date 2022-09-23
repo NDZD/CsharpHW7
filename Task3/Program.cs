a547929105150484c772def113348ccf70b7aff0
NDZD <n_level@mail.ru>
2022-09-23 14:13:13 +0300

    Task3

--- Task3/Program.cs
+++ Task3/Program.cs
@@ -13,13 +13,13 @@ int m = Convert.ToInt32(Console.ReadLine());
 System.Console.WriteLine("Введите количество элементов в столбце: ");
 int n = Convert.ToInt32(Console.ReadLine());
 
-int GetResult(int m, int n)
+double GetResult(int m, int n)
 {
     
 int [,] arr = new int [m,n];
 int [] result = new int [m];
-int sum = 0;
-int res = 0;
+double sum = 0;
+double res = 0;
     for (int i = 0; i < m; i++){
         for (int j = 0; j < n; j++){
             arr [i,j] = new Random().Next(10);
@@ -31,18 +31,20 @@ int res = 0;
     System.Console.WriteLine();
 
     for (int i = 0; i < n; i++){
-        sum += arr[i,j];
+        
         for (int j = 0; j < m; j++){
             
-            
+            sum += arr[j,i];
             //System.Console.Write();
         }
         res = sum / m;
-        System.Console.Write(sum);
-        System.Console.Write(res);
+        System.Console.Write("Сумма элементов в столбце " + sum + " ");
+        System.Console.Write("Среднее арифметическое " + res);
         System.Console.WriteLine();
+        sum = 0;
     }
     return res;
+    
     
 }
 GetResult(m,n);
